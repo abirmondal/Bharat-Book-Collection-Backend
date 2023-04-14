4302c5f46f650f156b46d6e3e02fdae90fcf2be4
abirmondal <abir22197072@gmail.com>
2023-04-14 17:48:36 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
@@ -9,9 +10,10 @@ load_dotenv()
 dbHost = os.getenv('DB_HOST')
 dbName = os.getenv('DB_NAME')
 dbUser = os.getenv('DB_USERNAME')
-dbPass = os.getenv('DB_PASSWORD')
+dbPass = urllib.parse.quote(os.getenv('DB_PASSWORD'))
 
-DATABASE_URL = "mysql+mysqlconnector://"+ dbUser + ":" + dbPass +"@"+ dbHost +":3306/" + dbName
+DATABASE_URL = "mysql+mysqlconnector://" + dbUser + \
+    ":" + dbPass + "@" + dbHost + ":3306/" + dbName
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
