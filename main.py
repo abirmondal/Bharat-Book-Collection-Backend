5f9c21771fc3420e1d75cda13cf0e4c37a3108d2
abirmondal <abir22197072@gmail.com>
2023-04-15 09:38:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from fastapi.middleware.cors import CORSMiddleware
 from DBoperations import *
@@ -51,8 +51,8 @@ async def docs_redirect():
     return response
 
 @app.post("/login/", tags=['users'])
-def loginAuth(username: str, password: str):
-    return checkUser(username, password)
+def loginAuth(user: UserLogin):
+    return checkUser(user.username, user.password)
 
 
 @app.post("/signup/", tags=['users'])
--- models.py
+++ models.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
 class UserDet(BaseModel):
     username: str
     name: str
